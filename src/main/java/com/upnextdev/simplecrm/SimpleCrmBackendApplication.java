883cbae9f047d6823666a19d22aba5ea840bde89
dtchin2 <dtchin2@gmail.com>
2023-12-31 02:28:20 -0500

    Added code changes to customer repo to pull one customer for email, allowed for cross origin

--- src/main/java/com/upnextdev/simplecrm/SimpleCrmBackendApplication.java
+++ src/main/java/com/upnextdev/simplecrm/SimpleCrmBackendApplication.java
@@ -2,6 +2,7 @@ package com.upnextdev.simplecrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SimpleCrmBackendApplication {
